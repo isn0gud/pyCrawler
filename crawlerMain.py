cb29e85e2e2187df76dfc191a57f816f494a27f5
pFriesch <p.friesch@posteo.de>
2014-04-22 11:17:15 +0200

    added page graph

--- crawlerMain.py
+++ crawlerMain.py
@@ -5,6 +5,12 @@ from bs4 import BeautifulSoup
 
 __author__ = 's0540030,s0540040,s054XXX'
 
+d = 0.95
+
+
+def pageRank(graph):
+
+
 def main(argv):
     listToVisit = ["http://mysql12.f4.htw-berlin.de/crawl/d01.html",
                "http://mysql12.f4.htw-berlin.de/crawl/d06.html",
@@ -12,17 +18,25 @@ def main(argv):
 
 
     listVisited = []
+    graph = dict()
     while listToVisit:
         url = listToVisit.pop()
         file = request.urlopen(url)
         listVisited.append(url)
         soup = BeautifulSoup(file)
-    for link in soup.find_all('a'):
-        newUrl = link.get('href')
-        newUrl = parse.urljoin(url, newUrl)
-    if not (newUrl in listToVisit) and not (newUrl in listVisited): listToVisit.append(newUrl)
+        for link in soup.find_all('a'):
+            newUrl = link.get('href')
+            newUrl = parse.urljoin(url, newUrl)
+            if (url in graph) and not (newUrl == url):
+                graph[url].append(newUrl)
+            else:
+                graph[url] = list([newUrl])
+            if not (newUrl in listToVisit) and not (newUrl in listVisited): listToVisit.append(newUrl)
     for item in listVisited: print(item)
-    if __name__ == "__main__": main(sys.argv)
+    print('\n')
+    print(graph)
+if __name__ == "__main__": main(sys.argv)
 
 
-main()
+
+
